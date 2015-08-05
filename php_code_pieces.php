bd1b0538ae49236b9c363bcd7dcd0b1285dea3a6
VS <viveks@elayers.net>
2015-08-06 01:21:43 +0530

    Create php_code_pieces.php

---
+++ php_code_pieces.php
@@ -0,0 +1,9 @@
+<?php
+## Print/Dump variable's contents and stop execution
+echo "<pre/>";print_r($variable);exit;
+### or ###
+echo "<pre/>";print_r($variable);die;
+### or ###
+echo "<pre/>";var_dump($variable);die;
+### or ###
+echo "<pre/>";var_dump($variable);exit;
