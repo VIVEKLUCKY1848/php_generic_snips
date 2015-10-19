ed12c354b1527f9544c72c611f3601e1fbdb1486
VS <viveks@elayers.net>
2015-10-19 17:31:35 +0530

    Create pathswap.php

---
+++ pathswap.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+<script type="text/javascript">
+function copyToClipboard(text) {
+	window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
+</script>
+</head>
+<body>
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["basePath"])) {
+	 $nameErr = "Basepath cannot be empty";
+	}
+
+	$basePath = $_POST['basePath'];
+	$pathData = parse_url($basePath);
+
+	$ip = $pathData['host'];
+	## echo "<pre/>";print_r($pathData);die;
+	$uri = ltrim ($pathData['path'],'/');
+
+	if($pathData['scheme'] == "smb")
+		$finalPath = "/run/user/1000/gvfs/smb-share:server=$ip,share=$uri";
+	elseif($pathData['scheme'] == "ftp")
+		$finalPath = "/run/user/1000/gvfs/ftp:host=$ip/$uri";
+}
+?>
+
+<h2>Convert path from SMB/FTP to RUN GVFS Linux</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+   Base Path: <input type="text" name="basePath" value="<?php echo $basePath;?>">
+   <span class="error">* <?php echo $nameErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Convert">
+</form>
+
+<?php
+if($finalPath):
+	echo "<h2>Converted Run GVFS path:</h2>";
+	$magePath = strstr($finalPath, 'app/');
+	echo "<script type='text/javascript'>copyToClipboard('".$finalPath."');</script>";
+
+	if($magePath != '')
+		echo "<script type='text/javascript'>copyToClipboard('".$magePath."');</script>";
+endif;
+?>
+</body>
+</html>
+<?php exit; ?>
